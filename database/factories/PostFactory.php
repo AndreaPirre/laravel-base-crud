dfd9b37d29725b0bdb15fa2b6b9b315329d9d7f7
Andrea <andreapirrello1990@gmail.com>
2020-09-29 18:38:18 +0200

    lanciato comando seed

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -9,10 +9,10 @@ use App\Post;
 
 $factory->define(Post::class, function (Faker $faker) {
     return [
-        "title"       =>  $faker -> title();
-        "category"    =>  $faker -> sentence();
-        "text"        =>  $faker -> text($maxNbChars = 100);
-        "like"        =>  $faker -> numberBetween($min = 0, $max = 10000);
-        "dislike"     =>  $faker -> numberBetween($min = 0, $max = 10000)
+        'title'    =>  $faker -> title(),
+        'category' =>  $faker -> sentence(),
+        'text'     =>  $faker -> text($maxNbChars = 100),
+        'like'     =>  $faker -> numberBetween($min = 0, $max = 100),
+        'dislike'  =>  $faker -> numberBetween($min = 0, $max = 100)
     ];
 });
